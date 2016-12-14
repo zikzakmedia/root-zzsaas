387f02d9430fd083d2b1cd9863fbe5efb8bd6675
Raimon Esteve <raimon@nan-tic.com>
2016-12-15 00:16:45 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s ~/.virtualenvs/${PWD##*/}/bin .
+hg clone ssh://hg@bitbucket.org/zikzakmedia/tryton-tasks tasks
+hg clone ssh://hg@bitbucket.org/zikzakmedia/tryton-config config -b 4.2
+pip install -r tasks/requirements.txt
+pip install -r config/requeriments.txt
+invoke modules.clone -c base.cfg
+invoke modules.clone
---
+++ virtualcreate.sh
@@ -0,0 +1,1 @@
+mkvirtualenv -p ~/pypy2-v5.6.0-linux64/bin/libpypy-c.so $1
