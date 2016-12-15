01d12789a76c81f008d4bd136b4733ebf47ff78b
Raimon Esteve <raimon@nan-tic.com>
2016-12-15 07:39:37 +0100

    Upgrade boostrap and virtualcreate

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s ~/.virtualenvs/${PWD##*/}/bin .
+#ln -s ~/.virtualenvs/${PWD##*/}/bin .
 hg clone ssh://hg@bitbucket.org/zikzakmedia/tryton-tasks tasks
 hg clone ssh://hg@bitbucket.org/zikzakmedia/tryton-config config -b 4.2
 pip install -r tasks/requirements.txt
