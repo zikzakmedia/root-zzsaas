868d4f24f59db9bea1d2151fabec28ee464b593b
Raimon Esteve <raimon@nan-tic.com>
2016-12-20 19:22:36 +0100

    Add no site packages option in create virtualenv

--- mkvirtualcreate.sh
+++ mkvirtualcreate.sh
@@ -1,1 +1,1 @@
-mkvirtualenv -p /home/$USER/pypy2-v5.6.0-linux64/bin/pypy $1
+mkvirtualenv -p /home/$USER/pypy2-v5.6.0-linux64/bin/pypy --no-site-packages $1
--- virtualcreate.sh
+++ virtualcreate.sh
@@ -1,1 +1,1 @@
-virtualenv -p /home/$USER/pypy2-v5.6.0-linux64/bin/pypy $1
+virtualenv -p /home/$USER/pypy2-v5.6.0-linux64/bin/pypy --no-site-packages $1
