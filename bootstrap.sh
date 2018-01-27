01843021be8590da1312e83051b270ef1fa8a3ff
Raimon Esteve <raimon@nan-tic.com>
2018-01-27 09:25:00 +0100

    Add create local.cfg in boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ pip install simpleeval
 
 hg clone ssh://hg@bitbucket.org/zikzakmedia/trytontasks tasks
 hg clone ssh://hg@bitbucket.org/zikzakmedia/tryton-config config -b default
+touch local.cfg
 
 pip install -r tasks/requirements.txt
 pip install -r config/requirements.txt
