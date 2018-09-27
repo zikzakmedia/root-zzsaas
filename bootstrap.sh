819c65d83fbae8646054d56538347b8741e6c3cb
Raimon Esteve <raimon@nan-tic.com>
2018-09-27 08:29:04 +0200

    Allow project and version in boostrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 #ln -s ~/.virtualenvs/${PWD##*/}/bin .
-#pip install psycopg2cffi
+
+if [ -z "$1" ]; then
+    echo "$0 branch (project)"
+    exit 0
+fi
+
 pip install --upgrade pip
 pip install simpleeval
 
 hg clone ssh://hg@bitbucket.org/zikzakmedia-erp/trytontasks tasks
-hg clone ssh://hg@bitbucket.org/zikzakmedia-erp/tryton-config config -b 4.8
+hg clone ssh://hg@bitbucket.org/zikzakmedia-erp/tryton-config config -b $1
 touch local.cfg
 
 pip install -r tasks/requirements.txt
 pip install -r config/requirements.txt
-pip install -r requirements.txt
 
 invoke modules.clone -c base.cfg
+
+if [ ! -z "$2" ]; then
+    hg clone ssh://zikzak@hg.zzsaas.com//home/tryton/master/clients/$2/ modules/$2 -b $1
+    rm local.cfg
+    ln -s modules/$1/local.cfg .
+    ln -s modules/$1/requirements.txt .
+    pip install -r requirements.txt
+fi
 invoke modules.clone
 quilt push -a
 
